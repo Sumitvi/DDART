3f2a73be03c31d53e74f6b4a3e26975b62949e6c
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-28 14:50:06 +0530

    Inheritance

---
+++ Inheritance.dart
@@ -0,0 +1,25 @@
+class Parent {
+  String Name = "Saksham";
+  String caste = "Vishwakarna";
+  String color = "White";
+  double height = 5.6;
+
+  void Happy() {
+    print("We Are Happy Family");
+  }
+}
+
+class Child extends Parent {
+  String name = "Shaurya";
+
+  void education() {
+    print("Pursing Engineering");
+  }
+}
+
+void main() {
+  Child c1 = new Child();
+  print(c1.caste);
+  print(c1.name);
+  c1.Happy();
+}
