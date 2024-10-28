dad715672ace04bb1730b482b35da6326152c50d
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-28 14:09:55 +0530

    Static keyword

---
+++ Static.dart
@@ -0,0 +1,13 @@
+class Constants {
+  static String greeting = "Hello Sir";
+  static String bye = "Bye Sir";
+}
+
+void main() {
+  print(Constants.greeting);
+  print(Constants.bye);
+}
+
+
+
+// In Static Variables The Objects do'nt need to created so the Constructor does'nt created and that's why we don'nt get extra memory losses
