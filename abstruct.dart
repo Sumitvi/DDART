a70401f64643b2696ed8cdcffc3011adf4d7672b
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-29 15:20:35 +0530

    Abstruct Class

--- abstruct.dart
+++ abstruct.dart
@@ -12,6 +12,7 @@ void main() {
 abstract class Animal {
   // Abstract class ek template ki tarah hoti hai. Ye define karti hai ki derived (child) classes ko kya methods ya properties implement karni hain, lekin khud ye methods ko implement nahi karti.
   void makeSound();
+  int noofLegs = 4;
 }
 
 class Dog extends Animal {
@@ -22,13 +23,13 @@ class Dog extends Animal {
 
 class Cat extends Animal {
   void makeSound() {
-    print("Meow");
+    print("Meow $noofLegs");
   }
 }
 
-class Elephant implements Animal {
+class Elephant extends Animal {
   @override
   void makeSound() {
-    print("HaHaHaHaHa");
+    print("HaHaHaHaHa $noofLegs");
   }
 }
