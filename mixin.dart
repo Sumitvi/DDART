368611694cf1286a702193bd6aa444601830b97c
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-30 12:22:38 +0530

    Mixin

--- mixin.dart
+++ mixin.dart
@@ -2,14 +2,28 @@ void main() {
   Animal Anim = Animal();
 
   Anim.fn();
+
+  Anim.Printskirry();
 }
 
 mixin Jump {
   int jumping = 10;
 }
 
-class Animal with Jump {
+mixin Skirry {
+  bool skirry = false;
+}
+
+class Animal with Jump, Skirry {
   void fn() {
     print(jumping);
   }
+
+  void Printskirry() {
+    print(skirry);
+  }
 }
+
+
+// Mixin is similar as the Inheritance but it does'nt create any Parent . Child Relationship
+// We Can Create multiple Mixin with class
