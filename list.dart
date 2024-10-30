d39030f885952ccedf59e5726349d0be46c46438
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-30 13:15:56 +0530

    List

--- list.dart
+++ list.dart
@@ -1,16 +1,35 @@
+// import 'dart:html_common';
+
 void main() {
   // this list for weather the value is string , boolean etc...
   List list = [10, 23, 60, "Sumit", 10.34, false];
-  print(list[0]);
-  print(list[1]);
-  print(list[2]);
+  // print(list[0]);
+  // print(list[1]);
+  // print(list[2]);
 
-  // But if i need only integer value
+  print(list.length);
+  print(list.isNotEmpty);
+  print(list.remove(10));
+  print(list);
+
+  print(list.indexOf(false));
+
+  // Itrating List
+
+  for (int i = 0; i < list.length; i++) {
+    print(list[i]);
+  }
+
+  // list.map((lists) => print(lists)).toList();
+      
+      
+      
+       // But if i need only integer value
   // So What i need to Do??
 
   List<int> list1 = [10, 24, 56, 87];
 
   // <> ---> it is called Generics
 
-  print(list1[3]);
+  // print(list1[3]);
 }
