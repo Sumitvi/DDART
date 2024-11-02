c818200d9456ba165ff37bbdfe02c7746677305a
Sumit Vishwakarma <sv0042595@gmail.com>
2024-11-02 12:33:48 +0530

    New Commit

---
+++ Revision/Statickeyword.dart
@@ -0,0 +1,17 @@
+class Constants {
+  static String greeting = "Hello Sir";
+  static String bye = "Bye Sir";
+
+  static int getValue() {
+    return 10;
+  }
+}
+
+void main() {
+  print(Constants.greeting);
+  print(Constants.bye);
+  print(Constants.getValue());
+}
+
+
+
